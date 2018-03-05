1064760beaadcb9698c1f4044159f8e55828551c
Kelsey Fecho <kfecho30@gmail.com>
2018-03-05 02:51:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+  
   WIN_COMBINATIONS = [
       [0,1,2], #top row win
       [3,4,5], #middle row win
@@ -12,5 +13,11 @@ class TicTacToe
       [0,4,8], #diagonal win
       [2,4,6] #diagonal win
     ]
-
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{b@oard[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 end
